0f20923b343d7236e31b3998064d2d563215c494
manavc7 <155104650+manavc7@users.noreply.github.com>
2024-05-05 00:07:11 +0100

    Update tip.py

--- cs50/CS50P 2022/Problem Set 0/tip.py
+++ cs50/CS50P 2022/Problem Set 0/tip.py
@@ -1,3 +1,4 @@
+#program that takes the cost of a meal in dollars and the percentage of tip to leave then outputs the amount to tip in dollars
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
