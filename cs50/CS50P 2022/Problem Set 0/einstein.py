918a80b479eb218d4f26833b19c697ba724b9338
manavc7 <155104650+manavc7@users.noreply.github.com>
2024-05-05 00:41:00 +0100

    Update einstein.py

--- cs50/CS50P 2022/Problem Set 0/einstein.py
+++ cs50/CS50P 2022/Problem Set 0/einstein.py
@@ -1,2 +1,3 @@
+#user inputs mass as an integer (in kilograms) and program outputs the equivalent number of Joules as an integer assuming that the user will input an integer.
 m = input("m: ")
 print("E = " + str(int(m) * (300000000**2)))
