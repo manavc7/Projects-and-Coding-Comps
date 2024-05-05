a36b25f88491447d92bb32335909233fc449af48
manavc7 <155104650+manavc7@users.noreply.github.com>
2024-05-05 01:23:37 +0100

    Update twttr.py

--- cs50/CS50P 2022/Problem Set 3/twttr.py
+++ cs50/CS50P 2022/Problem Set 3/twttr.py
@@ -1,3 +1,4 @@
+#program that prompts the user for a str of text and then outputs that same text but with all vowels (A, E, I, O, and U) omitted, whether inputted in uppercase or lowercase.
 text = input("Input: ")
 
 for i in ['A','E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']:
