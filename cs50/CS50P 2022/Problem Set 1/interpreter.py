97580988eaf8b1ddf2b3565ffa8848e1efff38d0
manavc7 <155104650+manavc7@users.noreply.github.com>
2024-05-05 01:04:35 +0100

    Update interpreter.py

--- cs50/CS50P 2022/Problem Set 1/interpreter.py
+++ cs50/CS50P 2022/Problem Set 1/interpreter.py
@@ -1,3 +1,8 @@
+#program that prompts the user for an arithmetic expression and then calculates and outputs the result as a floating-point value formatted to one decimal place. Assume that the user’s input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
+#x is an integer
+#y is +, -, *, or /
+#z is an integer
+
 expression = input("Expression: ")
 expression = expression.split()
 
