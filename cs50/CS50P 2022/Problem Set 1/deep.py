3a8b9db967414c1b5a79d98bea228940a4c80199
manavc7 <155104650+manavc7@users.noreply.github.com>
2024-05-05 01:01:20 +0100

    Update deep.py

--- cs50/CS50P 2022/Problem Set 1/deep.py
+++ cs50/CS50P 2022/Problem Set 1/deep.py
@@ -1,3 +1,4 @@
+#program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise outputs No
 ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
 
 if ans.strip() == "42" or ans.lower() == "forty two" or ans.lower() == "forty-two":
