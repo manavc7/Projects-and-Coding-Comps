33038d45c3c90336a18877737d30e64f58a55db8
manavc7 <155104650+manavc7@users.noreply.github.com>
2024-05-05 01:20:41 +0100

    Update coke.py

--- cs50/CS50P 2022/Problem Set 3/coke.py
+++ cs50/CS50P 2022/Problem Set 3/coke.py
@@ -1,3 +1,5 @@
+#program that prompts the user to insert a coin, one at a time, each time informing the user of the amount due. Once the user has inputted at least 50 cents, output how many cents in change the user is owed assuming that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
+
 due = 50
 paid = 0
 while paid < due:
