94640bcba447110f85fc3fc96f8cee462374eb0d
manavc7 <155104650+manavc7@users.noreply.github.com>
2024-05-05 01:02:30 +0100

    Update bank.py

--- cs50/CS50P 2022/Problem Set 1/bank.py
+++ cs50/CS50P 2022/Problem Set 1/bank.py
@@ -1,3 +1,4 @@
+#program that prompts the user for a greeting. If the greeting starts with “hello”, outputs $0. If the greeting starts with an “h” (but not “hello”), outputs $20. Otherwise, outputs $100
 greeting = input("Greeting: ")
 greeting = greeting.strip()
 
